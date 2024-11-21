1905f22d1115b830cbac3a23d768fc60736c6059
ew42 <evan@craigwall.com>
2024-11-21 11:21:07 -0500

    fuck shit

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const index = require('./routes/index.js');
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, '..', 'frontend', 'build')));
-
-app.get('/', (req, res) => {
-  console.log("Fn");
-  res.sendFile(path.join(__dirname, '..', 'frontend', 'build', 'index.html'));
-});
+app.use('/', index);
 
 module.exports = app;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', '..', 'frontend', 'build', 'index.js'));
+  res.sendFile(path.join(__dirname, '..', '..', 'frontend', 'build', 'index.html'));
 });
 
 module.exports = router;
