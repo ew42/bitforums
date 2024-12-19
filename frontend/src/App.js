536f34462931c77e0e8900b6d09218447dab9a4c
ew42 <evan@craigwall.com>
2024-12-19 12:45:24 -0600

    Start RootContainer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import './global.css';
+import RootContainer from './components/RootContainer.js';
 import NavBar from './components/NavBar.js';
 
 function App() {
   return (
     <div className="App">
-      < NavBar/>
+      < RootContainer/>
     </div>
   );
 }
--- frontend/src/components/RootContainer.js
+++ frontend/src/components/RootContainer.js
@@ -1,8 +1,65 @@
-import React from 'react';
+import React, { useState}from 'react';
+import "./RootContainer.css";
 
-const RootContainer = () => {
+
+//What are the rules for the control panels?
+//Need to distinguish between panes and tabs
+//Pane needs to have a state that is number of tabs
+
+//const TabController = ({ title , number}) => {
+//  return (
+//    <div className="tab-controller">
+//      <p className="tab-controller-title">{title}</p>
+//      <button className="tab-x-button" onClick={( {title} ) => {
+//
+//        }}>X</button>
+//    </div>
+//)};
+
+const PaneControlPanel = ({ paneStates, togglePanes}) => {
+
   return (
-    
+    <div className="pane-control-panel">
+      <div className="button-div">
+        <div className="left-pane-div">
+          <button className="left-button" onClick={() => {togglePanes(0)}}>Left Pane</button>
+        </div>
+        <div className="right-pane-div">
+          <button className="right-button" onClick={() => {togglePanes(1)}}>Right Pane</button>
+        </div>
+      </div>
+    </div>
 )};
 
-export default RootContainer
+const Pane = ({ title="test" }) => {
+  return (
+  <div className="pane">
+    <h1>{title}</h1>
+  </div>
+)};
+
+const RootContainer = () => {
+
+  const [paneStates, setPaneStates] = useState([true, true]);
+
+  const togglePane = (index) => {
+    setPaneStates(prevPanes => {
+      const newPanes = [...prevPanes];
+      newPanes[index] = !newPanes[index];
+      return newPanes;
+    })
+  };
+
+  return (
+  <div className="RootContainer">
+    <PaneControlPanel paneStates={paneStates} togglePanes={togglePane}/>
+    <div className="pane-container">
+        {paneStates[0] && (
+        <Pane title="Left Pane"/>)}
+        {paneStates[1] && (
+        <Pane title="Right Pane"/>)}
+    </div>
+  </div>
+)};
+
+export default RootContainer;
