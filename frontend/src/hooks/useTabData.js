17364c18c41e03189d0278ee0a692995ec266957
ew42 <evan@craigwall.com>
2025-01-22 09:53:16 -0500

    Small fix

--- frontend/src/hooks/useTabData.js
+++ frontend/src/hooks/useTabData.js
@@ -6,7 +6,7 @@ const useTabData = () => {
 
   //tab stuff
   const [tabs, setTabStates] = useState({
-    left: [{title: "Forum Browser", visible: false, type: "forum browser"}, {title: "Welcome to Bitforums", visible: true, type: "forum", id="677b35b26bf10ef201ed7170"}],
+    left: [{title: "Forum Browser", visible: false, type: "forum browser"}, {title: "Welcome to Bitforums", visible: true, type: "forum", id:"677b35b26bf10ef201ed7170"}],
     central: [{title: "Editor", visible: true, type: "viewer"}],
     right: [{title: "top posts", visible: true, type: "graph view", id: "67819bf3cc0854c19f831f6d"}]
   });
